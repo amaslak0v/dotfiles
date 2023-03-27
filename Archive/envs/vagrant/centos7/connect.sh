04e9b5c7eeff0d32709b1ca8f0aba2a24ae062bb
Anton Maslakov <amaslakov@icloud.com>
2023-03-27 17:48:52 +0800

    deleting Archive

--- Archive/envs/vagrant/centos7/connect.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh vagrant@192.168.10.10 -i .vagrant/machines/default/virtualbox/private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PasswordAuthentication=no -o IdentitiesOnly=yes
