afdd000b83333b70e4bb26ed650c0a4a49a6c8f2
Tony M <amaslakov@icloud.com>
2023-03-27 17:41:36 +0800

    Deleting Archive

--- Archive/envs/docker/jenkins-from-scratch/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting docker container" 
-read -p  "Enter (centos-jenkins): " container 
-echo "Container : ${container:=centos-jenkins}"
-docker run -p 8080:8080 ${container}
-
-DockerId="$(docker ps -q)"
-docker ps
-
--- Archive/envs/docker/jenkins-from-scratch/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Stoping docker container" 
-DockerId="$(docker ps -q)"
-read -p  "Enter (${DockerId}): " container
-echo "Container : ${container:=${DockerId}}"
-docker stop ${DockerId}
-docker ps
-
